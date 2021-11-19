56dd234b9ca2b971c2ce0fb67483c16962a6c024
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-11-19 23:40:20 +0300

    Add login logic to app.js

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 require("dotenv").config();
 require("./config/database").connect();
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
 const express = require("express");
 const User = require("./model/user");
+const auth = require("./middleware/auth");
 
 const app = express();
 
 app.use(express.json());
 
-// App Logic will be here
+app.get("/welcome", auth, (req, res) => {
+  res.status(200).send("Welcome 🙌 ");
+});
+
+// Register Logic
 app.post("/register", async (req, res) => {
   try {
     const { first_name, last_name, email, password } = req.body;
@@ -16,7 +23,7 @@ app.post("/register", async (req, res) => {
       res.status(400).send("All fields are required.");
     }
 
-    const oldUser = await User.find({ email });
+    const oldUser = await User.findOne({ email: email });
 
     if (oldUser) {
       res.status(409).send("User Already Exist. Please Login");
@@ -25,7 +32,7 @@ app.post("/register", async (req, res) => {
 
     const encryptedPassword = await bcrypt.hash(password, 10);
 
-    const user = User.create({
+    const user = await User.create({
       first_name,
       last_name,
       email: email.toLowerCase(),
@@ -45,10 +52,45 @@ app.post("/register", async (req, res) => {
     // return new user
     res.status(201).json(user);
   } catch (err) {
-    console.log(err.message);
+    console.log(err);
   }
 });
 
-app.post("/login", (req, res) => {});
+// Login Logic
+app.post("/login", async (req, res) => {
+  // Our login logic starts here
+  try {
+    // Get user input
+    const { email, password } = req.body;
+
+    // Validate user input
+    if (!(email && password)) {
+      res.status(400).send("All input is required");
+    }
+    // Validate if user exist in our database
+    const user = await User.findOne({ email });
+
+    if (user && (await bcrypt.compare(password, user.password))) {
+      // Create token
+      const token = jwt.sign(
+        { user_id: user._id, email },
+        process.env.TOKEN_KEY,
+        {
+          expiresIn: "2h",
+        }
+      );
+
+      // save user token
+      user.token = token;
+
+      // user
+      res.status(200).json(user);
+    }
+    res.status(400).send("Invalid Credentials");
+  } catch (err) {
+    console.log(err);
+  }
+  // Our register logic ends here
+});
 
 module.exports = app;
