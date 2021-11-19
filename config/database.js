b5f91a0a8a00cf1422908bb2f67f363364880535
Fatih <68196068+fatihozoglu@users.noreply.github.com>
2021-11-19 22:04:19 +0300

    Add mongodb connection with mongoose

---
+++ config/database.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+// Getting MONGO_URI environment variable from .env file
+const { MONGO_URI } = process.env;
+
+exports.connect = async () => {
+  try {
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+    });
+
+    console.log("Successfully connected to database");
+  } catch (err) {
+    console.log("Database connection failed. Exiting now...");
+    console.error(err.message);
+    process.exit(1);
+  }
+};
